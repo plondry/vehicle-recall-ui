aa132f9f62cec3482995a81ef9356c36d347db5a
MubarakOseni <mubarakoseni@gmail.com>
2022-03-16 20:43:36 -0400

    added condition for api search

--- app/src/api/index.js
+++ app/src/api/index.js
@@ -21,5 +21,10 @@ export function getVehicleRecallsWithURL(baseURL) {
     return axios.get(baseURL);
 }
 export function searchVehicleRecallsWithURL(baseURL, searchValue) {
+    if (baseURL=="http://localhost:3001/v1/api/vehicle-recalls"){
+        var searchparam="/?manufacturer_recall_no_txt="
+        return axios.get(baseURL+searchparam+searchValue);
+    }
     return axios.get(baseURL+"/search?value="+searchValue);
+ 
 }
