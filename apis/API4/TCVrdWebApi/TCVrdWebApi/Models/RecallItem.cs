f33cd9d07ff2115f7e0f0d78bfba7141a4e6fbdb
woxingren <37563176+woxingren@users.noreply.github.com>
2022-03-17 00:08:50 -0400

    add json property mapping

--- apis/API4/TCVrdWebApi/TCVrdWebApi/Models/RecallItem.cs
+++ apis/API4/TCVrdWebApi/TCVrdWebApi/Models/RecallItem.cs
@@ -1,18 +1,42 @@
-﻿namespace TCVrdWebApi.Models
+﻿using Newtonsoft.Json;
+namespace TCVrdWebApi.Models
 {
     public class RecallItem
     {
-        public string recallNumber { get; set; }
-        public string manufactureName { get; set; }
-        public string makeName { get; set; }
-        public string modelName { get; set; }
-        public string recallYear { get; set; }
-        public string manufacturerRecallNoTxt { get; set; }
-        public string categoryEtxt { get; set; }
-        public string categoryFtxt { get; set; }
-        public string systemTypeEtxt { get; set; }
-        public string systemTypeFtxt { get; set; }
-        public string notificationTypeEtxt { get; set; }
-        public string notificationTypeFtxt { get; set; }     
+        [JsonProperty("recallNumber")]
+        public string RecallNumber { get; set; }
+
+        [JsonProperty("manufactureName")]
+        public string ManufactureName { get; set; }
+
+        [JsonProperty("makeName")]
+        public string MakeName { get; set; }
+
+        [JsonProperty("modelName")]
+        public string ModelName { get; set; }
+
+        [JsonProperty("recallYear")]
+        public string RecallYear { get; set; }
+
+        [JsonProperty("manufacturerRecallNoTxt")]
+        public string ManufacturerRecallNoTxt { get; set; }
+
+        [JsonProperty("categoryEtxt")]
+        public string CategoryEtxt { get; set; }
+
+        [JsonProperty("categoryFtxt")]
+        public string CategoryFtxt { get; set; }
+
+        [JsonProperty("systemTypeEtxt")]
+        public string SystemTypeEtxt { get; set; }
+
+        [JsonProperty("systemTypeFtxt")]
+        public string SystemTypeFtxt { get; set; }
+
+        [JsonProperty("notificationTypeEtxt")]
+        public string NotificationTypeEtxt { get; set; }
+
+        [JsonProperty("notificationTypeFtxt")]
+        public string NotificationTypeFtxt { get; set; }     
     }
 }
